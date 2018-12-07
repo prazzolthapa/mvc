d87dae88cfa133bf23760fbebc8886ddd07dc603
prazzolthapa <thapaprazzol@gmail.com>
2018-12-07 13:17:29 +0000

    initial comment

---
+++ Config/settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prazzolthapa
+ * Date: 12/7/18
+ * Time: 12:56 PM
+ */
+settings=[
+    'url'=>'http://localhost/mvc',
+    'db_name'=>'',
+    'db_user'=>'',
+    'db_pass'=>'',
+    'db_host'=>'',
+    'timezone'=>'Asia/Kathmandu'
+
+];
